e7e0cba49abdbd9305ed24df915ade1d7d24b3bb
codebasedump <hello@hoffstee.com.au>
2025-04-11 09:40:56 +1000

    Mohan Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,14 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
+
+  // Preloader
+  $(window).on('load', function () {
+    $('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
+    $('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(333);
+});
+
   /**
    * Preloader
    */
