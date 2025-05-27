aa7bdc3e0f29733a35fedf12ce7fe7971688b6de
codebasedump <hello@hoffstee.com.au>
2025-05-27 14:23:31 +1000

    changes by mohan

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,4 +144,10 @@ for (let i = 0; i < acc.length; i++) {
   });
 }
 
-
+ const scrollContainer = document.getElementById('scrollContainer');
+    scrollContainer.addEventListener('wheel', function(event) {
+        if (event.deltaY !== 0) {
+              scrollContainer.scrollLeft += event.deltaY * 4; // Increase scroll distance by multiplying deltaY
+              event.preventDefault(); // Prevent the default vertical scroll behavior
+        }
+ }, { passive: false }); // Set passive to false to call preventDefault
