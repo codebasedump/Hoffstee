d1f155a7404e82e5e44266c225190177b508fbc4
codebasedump <hello@hoffstee.com.au>
2025-06-19 16:30:34 +1000

    fixes on chat box

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,16 +91,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-window.addEventListener('DOMContentLoaded', function () {
-    var s1 = document.createElement("script"),
-      s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = "https://embed.tawk.to/682ae6086392a3190c78079b/1irjp54gf";
-    s1.charset = "UTF-8";
-    s1.setAttribute("crossorigin", "*");
-    s0.parentNode.insertBefore(s1, s0);
-  });
-
 const images = document.querySelectorAll('.round_about');
 
 document.addEventListener('mousemove', (event) => {
