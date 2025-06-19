c9fc0f4ec398dcf4660c7db3b07a84f08c7c000e
codebasedump <hello@hoffstee.com.au>
2025-06-19 22:45:14 +1000

    image optimized

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         $('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
         $('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
         $('body').delay(333);
-    }, 500)
+    }, 100)
   });
 
   /**
